3ce645feb5bd01a8fa81f20e0b1201caf99d535d
Dennis Martin Cual <denniscual0716@gmail.com>
2024-08-12 14:05:54 +0800

    add prettier

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -8,7 +8,8 @@
     sources = {
       -- null_ls.builtins.diagnostics.eslint_d,
       null_ls.builtins.completion.spell,
-      null_ls.builtins.formatting.prettierd
+      null_ls.builtins.formatting.prettierd,
+      null_ls.builtins.formatting.prettier
     },
     on_attach = function(client, bufnr)
       if client.supports_method("textDocument/formatting") then
