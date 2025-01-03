05d0d4e94203250c35e108e1ffc071524f729fb5
Dennis Martin Cual <denniscual0716@gmail.com>
2025-01-03 21:45:50 +0800

    customize the highlight colors of TreesitterContext to make it less noticeable

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,7 +9,10 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+  vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#18151f" })
+  vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#18151f", fg = "#6e6a86" })
+  -- vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "#383344"})
+  -- vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "#383344"})
 end
 
 ColorMyPencils()
