b7eab31653d06e4023badf68b90f461014cd0093
Dennis Martin Cual <denniscual0716@gmail.com>
2025-01-04 16:17:37 +0800

    add lua to the format files pattern

--- after/plugin/none-ls.lua
+++ after/plugin/none-ls.lua
@@ -1,27 +1,28 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.stylua,
-  },
+	sources = {
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.stylua,
+	},
 })
 
 -- Format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = {
-    "*.js",
-    "*.jsx",
-    "*.ts",
-    "*.tsx",
-    "*.css",
-    "*.scss",
-    "*.html",
-    "*.json",
-    "*.md",
-  },
-  callback = function()
-    vim.lsp.buf.format()
-  end,
+	pattern = {
+		"*.js",
+		"*.jsx",
+		"*.ts",
+		"*.tsx",
+		"*.css",
+		"*.scss",
+		"*.html",
+		"*.json",
+		"*.md",
+		"*.lua",
+	},
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
