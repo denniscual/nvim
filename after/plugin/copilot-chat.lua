b7d6703b27c0bdaee69d510d532542900dcb5f58
Dennis Martin Cual <denniscual0716@gmail.com>
2025-01-05 15:12:51 +0800

    enhance key mappings for chat features

--- after/plugin/copilot-chat.lua
+++ after/plugin/copilot-chat.lua
@@ -2,4 +2,19 @@ local copilotChat = require("CopilotChat")
 
 copilotChat.setup()
 
-vim.api.nvim_set_keymap("n", "<leader>cc", "<cmd>CopilotChatToggle<CR>", { noremap = true, silent = true })
+-- Open Chat Window
+vim.api.nvim_set_keymap("n", "<leader>ccw", "<cmd>CopilotChatToggle<CR>", { noremap = true, silent = true })
+
+-- Open Quick Chat
+vim.keymap.set("x", "<leader>ccq", function()
+	local input = vim.fn.input("Quick Chat: ")
+	if input ~= "" then
+		require("CopilotChat").ask(input, { selection = require("CopilotChat.select").visual })
+	end
+end, { desc = "CopilotChat - Quick chat", noremap = true })
+
+-- Open Prompt Actions
+vim.keymap.set("x", "<leader>ccp", function()
+	local actions = require("CopilotChat.actions")
+	require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+end, { desc = "CopilotChat - Prompt actions", noremap = true })
