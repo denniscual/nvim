fd0c6eff4265780030fad6b9c55ec8ffe07982e5
Dennis Martin Cual <denniscual0716@gmail.com>
2025-01-07 00:03:26 +0800

    update telescope keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,17 +4,9 @@ vim.keymap.set("n", "<leader>fg", builtin.git_status, {})
 vim.keymap.set("n", "<C-p>", function()
 	builtin.find_files({ hidden = true })
 end)
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set("n", "<leader>fs", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
--- Show diagnostics from all open buffers.
-vim.keymap.set("n", "<leader>vg", builtin.diagnostics, {})
--- Show diagnostics from current buffer.
-vim.keymap.set("n", "<leader>vh", function()
-	builtin.diagnostics({ bufnr = 0 })
-end, { noremap = true, silent = true })
--- Show diagnostics all diagnostics in workspace.
--- vim.keymap.set('n', '<leader>vb', function()  builtin.diagnostics({ hide_filename = false, get_all = true }) end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fm", builtin.marks, {})
 
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
