f4aac73b39e0b508060f980b9dd2ca93e6d1aeed
Dennis Martin Cual <denniscual0716@gmail.com>
2025-01-10 14:45:57 +0800

    refactor telescope keybinds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,15 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.git_status, {})
+
+-- Keybinds:
+local leader_telescope = "<leader>f"
+
+vim.keymap.set("n", leader_telescope .. "f", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", leader_telescope .. "g", builtin.git_status, { desc = "Git status" })
 vim.keymap.set("n", "<C-p>", function()
 	builtin.find_files({ hidden = true })
-end)
-vim.keymap.set("n", "<leader>fs", function()
+end, { desc = "Find files (hidden included)" })
+vim.keymap.set("n", leader_telescope .. "s", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set("n", "<leader>fm", builtin.marks, {})
-
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+end, { desc = "Grep string" })
+vim.keymap.set("n", leader_telescope .. "m", builtin.marks, { desc = "Marks" })
+vim.keymap.set("n", leader_telescope .. "b", builtin.buffers, { desc = "Buffers" })
