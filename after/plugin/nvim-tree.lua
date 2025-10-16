d190da1307e2fc87011f928db563281cf106007f
Dennis Martin Cual <denniscual0716@gmail.com>
2025-10-16 11:41:16 +0800

    enable nvim tree adaptive size

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,2 +1,10 @@
+local nvim_tree = require("nvim-tree")
+
+nvim_tree.setup({
+	view = {
+		adaptive_size = true,
+	},
+})
+
 vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>")
 vim.keymap.set("n", "<leader>w", ":NvimTreeFindFile<CR>")
